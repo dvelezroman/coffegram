23e19ec9e20b026297c91306f8c1ee741633ea56
Dario Velez Roman <dvelezroman@Gmail.com>
2018-08-25 18:26:31 -0300

    Update firebase.js

--- src/Store/Services/firebase.js
+++ src/Store/Services/firebase.js
@@ -1,14 +1,7 @@
 import * as firebase from 'firebase';
 
 // Initialize Firebase
-const config = {
-  apiKey: 'AIzaSyA4DSEuX0_LhojLhhONG7PaD7b-DRY9sEw',
-  authDomain: 'caffeinagram.firebaseapp.com',
-  databaseURL: 'https://caffeinagram.firebaseio.com',
-  projectId: 'caffeinagram',
-  storageBucket: 'caffeinagram.appspot.com',
-  messagingSenderId: '1061388647207',
-};
+
 
 firebase.initializeApp(config);
 
