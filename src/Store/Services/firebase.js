388cf81da493b86863adabd12fe7fe512b330fc4
Dario <dvelezroman@gmail.com>
2018-08-26 22:38:05 -0300

    with firebase config

--- src/Store/Services/firebase.js
+++ src/Store/Services/firebase.js
@@ -12,7 +12,14 @@ console.warn = message => {
 };
 
 // Initialize Firebase
-
+var config = {
+  apiKey: "AIzaSyA4DSEuX0_LhojLhhONG7PaD7b-DRY9sEw",
+  authDomain: "caffeinagram.firebaseapp.com",
+  databaseURL: "https://caffeinagram.firebaseio.com",
+  projectId: "caffeinagram",
+  storageBucket: "caffeinagram.appspot.com",
+  messagingSenderId: "1061388647207"
+};
 
 firebase.initializeApp(config);
 
